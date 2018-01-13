11a2a2598adc74aeb7e9958f89d43be21a3dfa55
Franky Pear <FrankPear85@hotmail.com>
2018-01-13 14:28:03 +0100

    //Adding changes: +Application +Log +Module

---
+++ YunitiTresde/Application.h
@@ -0,0 +1,39 @@
+#ifndef __APPLICATION_CPP__
+#define __APPLICATION_CPP__
+
+#include<list>
+#include "Globals.h"
+#include "Module.h"
+//
+class ModuleRender;
+class ModuleWindow;
+class ModuleInput;
+
+class Application
+{
+public:
+
+	Application();
+	~Application();
+
+	bool Init();
+	update_status Update();
+	bool CleanUp();
+
+	//void RerunTimer();
+
+public:
+	ModuleRenderer* renderer;
+	ModuleWindow* window;
+	ModuleInput* input;
+
+	bool switchmap = false;
+private:
+
+	std::list<Module*> modules;
+
+};
+
+extern Application* App;
+
+#endif // __APPLICATION_CPP__
