0ebd8e703aacbf5ca72c49970c31108c0a015728
Oscar Lopez Fabregas <oscarlf2@ttc.cat>
2018-01-22 16:57:31 +0100

    Created ModuleImGui structure

---
+++ YunitiTresde/ModuleImGui.cpp
@@ -0,0 +1,24 @@
+#include "ModuleImGui.h"
+
+
+ModuleImGui::ModuleImGui()
+{
+}
+
+ModuleImGui::~ModuleImGui()
+{
+}
+
+bool ModuleImGui::Init()
+{
+	bool ret = true;
+
+
+
+	return ret;
+}
+
+update_status ModuleImGui::Update()
+{
+
+}
--- YunitiTresde/ModuleImGui.h
+++ YunitiTresde/ModuleImGui.h
@@ -1,1 +1,32 @@
-#pragma once
+#ifndef _MODULEIMGUI_
+#define _MODULEIMGUI_
+
+#include "Module.h"
+#include "Globals.h"
+#include "imgui-1.53\imgui.h"
+
+
+class ModuleImGui :
+	public Module
+{
+
+public:
+	ModuleImGui();
+	~ModuleImGui();
+
+	bool Init();
+	bool Start();
+
+	update_status Update();
+
+	bool CleanUp();
+
+
+private:
+
+};
+
+
+
+#endif // !_MODULEIMGUI_
+
