b0c49a05c235aa476dadce43969525a55e5be05a
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-01-23 16:03:19 +0100

    Modified imgui folder

--- YunitiTresde/ModuleImGui.cpp
+++ YunitiTresde/ModuleImGui.cpp
@@ -1,11 +1,17 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleWindow.h"
-#include <Windows.h>
+#include "SDL\include\SDL.h"
+
 #include "ModuleImGui.h"
+#include "OpenGL.h"
+
+#include "imgui-1.53\imgui.h"
 #include "imgui-1.53\imgui_impl_sdl_gl3.h"
+#include "imgui-1.53\glfw\include\GLFW\glfw3.h"
 
 
+#pragma comment (lib, "imgui-1.53/glfw/lib-vc2010-32/glfw3.lib")
 ModuleImGui::ModuleImGui()
 {
 }
