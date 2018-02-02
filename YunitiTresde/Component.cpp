9c04501cae4a162c173ed340bbebd5506dff63de
Max72590 <max-725@hotmail.com>
2018-02-02 19:06:29 +0100

    Committing component implementation update.

--- YunitiTresde/Component.cpp
+++ YunitiTresde/Component.cpp
@@ -9,4 +9,26 @@ Component::Component()
 
 Component::~Component()
 {
+
+}
+
+bool Component::IsEnabled() const
+{
+	return enabled;
+}
+void Component::Enable()
+{
+	if (!enabled) enabled = true;
+}
+void Component::Disable()
+{
+	if (enabled) enabled = false;
+}
+GameObject* Component::LinkedTo() const
+{
+	return linked_to;
+}
+Type Component::GetType() const
+{
+	return type;
 }
--- YunitiTresde/Component.h
+++ YunitiTresde/Component.h
@@ -1,8 +1,30 @@
 #pragma once
+
+class GameObject;
+
+enum Type {TRANSFORMATION,MATERIAL,MESH};
+
 class Component
 {
 public:
 	Component();
 	~Component();
+	bool IsEnabled() const;
+	void Enable();
+	void Disable();
+	GameObject* LinkedTo() const;
+	Type GetType() const;
+	
+	virtual bool PreUpdate() {}	
+	virtual bool Update() {}
+	virtual bool PostUpdate() {}
+	virtual bool Destroy() {}
+
+
+private:
+
+	Type type;
+	GameObject *linked_to;
+	bool enabled;
 };
 
