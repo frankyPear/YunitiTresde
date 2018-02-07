fc5bfe30d6fa8e646272ad4b0956f1e246eaa744
Alex <alexespinet@gmail.com>
2018-02-07 18:05:13 +0100

    Doing component material

--- YunitiTresde/ComponentMaterial.cpp
+++ YunitiTresde/ComponentMaterial.cpp
@@ -10,3 +10,23 @@ ComponentMaterial::ComponentMaterial()
 ComponentMaterial::~ComponentMaterial()
 {
 }
+
+void ComponentMaterial::SetMaterialResource(std::vector<MaterialResource*> materialResource)
+{
+	_materialResources = materialResource;
+}
+
+void ComponentMaterial::AddTexture(MaterialResource & materialResource)
+{
+	_materialResources.push_back(&materialResource);
+}
+
+void ComponentMaterial::ClearMaterialResource()
+{
+	unsigned int materialResourcesSize = _materialResources.size();
+	for (unsigned int i = 0; i < materialResourcesSize; i++) {
+
+	}
+	_materialResources.clear();
+
+}
--- YunitiTresde/ComponentMaterial.h
+++ YunitiTresde/ComponentMaterial.h
@@ -1,10 +1,36 @@
 #pragma once
 #include "Component.h"
+#include <vector>
+class MaterialResource;
+
 class ComponentMaterial :
 	public Component
 {
 public:
+
+	//Constructor
 	ComponentMaterial();
+
+
+	//Destructor
 	~ComponentMaterial();
+
+#pragma region Setters
+	void SetMaterialResource(std::vector<MaterialResource*> materialResource);
+
+
+#pragma endregion
+
+
+#pragma Component methods
+	void AddTexture(MaterialResource &materialResource);
+
+	void ClearMaterialResource();
+#pragma endregion
+private:
+	std::vector<MaterialResource*> _materialResources;
+
+
+
 };
 
