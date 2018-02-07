186cf81b1f20d9265111a55ae9ee11031bc3b3f6
Alex <alexespinet@gmail.com>
2018-02-07 18:12:05 +0100

    Revert

--- YunitiTresde/ComponentMaterial.h
+++ YunitiTresde/ComponentMaterial.h
@@ -1,36 +1,10 @@
 #pragma once
 #include "Component.h"
-#include <vector>
-class MaterialResource;
-
 class ComponentMaterial :
 	public Component
 {
 public:
-
-	//Constructor
 	ComponentMaterial();
-
-
-	//Destructor
 	~ComponentMaterial();
-
-#pragma region Setters
-	void SetMaterialResource(std::vector<MaterialResource*> materialResource);
-
-
-#pragma endregion
-
-
-#pragma Component methods
-	void AddTexture(MaterialResource &materialResource);
-
-	void ClearMaterialResource();
-#pragma endregion
-private:
-	std::vector<MaterialResource*> _materialResources;
-
-
-
 };
 
