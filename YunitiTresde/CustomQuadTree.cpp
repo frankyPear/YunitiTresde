19b16dc633ce1e06f1b2b4b35dc956470e0cf2d4
Max72590 <max-725@hotmail.com>
2018-02-10 21:21:51 +0100

    Added class, and node inside class, 90% of the functionalities done. Test pending.

---
+++ YunitiTresde/CustomQuadTree.cpp
@@ -0,0 +1,172 @@
+#include "CustomQuadTree.h"
+#include <stack>
+
+using namespace std;
+
+CustomQuadTree::CustomQuadTree()
+{
+
+}
+
+CustomQuadTree::~CustomQuadTree()
+{
+
+}
+
+void CustomQuadTree::Create(AABB limits)
+{
+	root_ = new CustomQuadTreeNode(limits, nullptr);
+}
+
+void CustomQuadTree::Clear() 
+{
+	delete root_;
+	root_ = nullptr;
+}
+
+void CustomQuadTree::Insert(GameObject *newAddition) 
+{
+	if (newAddition != nullptr) {
+		ComponentMesh* cm = (ComponentMesh*)newAddition->GetComponent(MESH);
+		ComponentTransform* ct = (ComponentTransform*)newAddition->GetComponent(TRANSFORMATION);
+		if (cm != nullptr && ct != nullptr) {
+			AABB newBox = *(cm->GetBoundingBox());
+			newBox.TransformAsAABB(ct->GetGlobalTransform());
+			if (root_ == nullptr) {
+				root_ = new CustomQuadTreeNode(newBox, root_);
+			}
+			else {
+				if (root_->box_.Contains(newBox)) root_->InsertInNode(newAddition);
+				else {
+					root_->box_.Enclose(newBox);
+					root_->InsertInNode(newAddition);
+				}
+			}
+		}
+	}
+}
+
+void CustomQuadTree::Remove(GameObject *toRemove) 
+{
+	if (root_ != nullptr && toRemove != nullptr) {
+		ComponentMesh* cm = (ComponentMesh*)toRemove->GetComponent(MESH);
+		ComponentTransform* ct = (ComponentTransform*)toRemove->GetComponent(TRANSFORMATION);
+		if (cm != nullptr && ct != nullptr) {
+			AABB newBox = *(cm->GetBoundingBox());
+			newBox.TransformAsAABB(ct->GetGlobalTransform());
+			if (root_ != nullptr) {
+				if (root_->box_.Contains(newBox)) root_->EliminateNode(toRemove);
+			}
+		}
+	}	
+}
+
+void CustomQuadTree::Intersect(std::vector<GameObject*>& toTest, const Frustum& camFrustum) 
+{
+	if (root_ != nullptr) root_->NodeIntersect( toTest, camFrustum);
+}
+
+//----------------------------- NODE FUNCTIONS ---------------------------------
+
+CustomQuadTreeNode::CustomQuadTreeNode()
+{
+
+}
+
+CustomQuadTreeNode::CustomQuadTreeNode(AABB box, CustomQuadTreeNode* parent) : box_(box), parent_(parent), child_nodes_()
+{
+
+}
+
+CustomQuadTreeNode::~CustomQuadTreeNode()
+{
+
+}
+
+void CustomQuadTreeNode::SetNodeBox(AABB box)
+{
+	box_ = box;
+}
+
+void CustomQuadTreeNode::SetNodeInChild(AABB box, int index)
+{
+	child_nodes_[index]->box_ = box;
+}
+
+void CustomQuadTreeNode::SetParent(CustomQuadTreeNode* newparent)
+{
+	parent_ = newparent;
+}
+
+void  CustomQuadTreeNode::InsertInNode(GameObject *toInsert)
+{
+	if (objectsInBox_.size() < MAX_OBJECTS_IN_NODE || box_.Size().x < MINIMUM_BOX_SIZE) objectsInBox_.push_back(toInsert);
+	else {
+		if (child_nodes_.empty()) DivideBox();
+		ReallocateChilds();
+	}
+}
+void CustomQuadTreeNode::EliminateNode(GameObject* toEliminate) 
+{
+	list<GameObject*>::iterator it;
+	bool found = false;
+	for (it = objectsInBox_.begin(); it != objectsInBox_.end() && !found; ++it) {
+		if (*it == toEliminate) {
+			found = true;
+			it = objectsInBox_.erase(it);
+		}
+	}
+	if (!found) {
+		for (int i = 0; i < 4; ++i) 
+		{
+			child_nodes_[i]->EliminateNode(toEliminate);
+		}
+	}
+}
+
+void CustomQuadTreeNode::NodeIntersect(std::vector<GameObject*>& toTest, const Frustum& camFrustum) 
+{
+
+}
+
+void CustomQuadTreeNode::ReallocateChilds()
+{
+
+	list<GameObject*>::iterator it;
+	for (it = objectsInBox_.begin(); it != objectsInBox_.end(); ++it){
+		for (int i = 0; i < 4; ++i) {
+			ComponentMesh* cm = (ComponentMesh*)(*it)->GetComponent(MESH);
+			ComponentTransform* ct = (ComponentTransform*)(*it)->GetComponent(TRANSFORMATION);
+			if (cm != nullptr && ct != nullptr) {
+				AABB newBox = *(cm->GetBoundingBox());
+				newBox.TransformAsAABB(ct->GetGlobalTransform());
+				if (child_nodes_[i]->box_.Intersects(newBox)) child_nodes_[i]->InsertInNode(*it);
+			}
+		}
+		it = objectsInBox_.erase(it);
+	}
+}
+
+void CustomQuadTreeNode::DivideBox() 
+{
+	float3 maxPoints = this->box_.maxPoint;
+	float3 minPoints = this->box_.maxPoint;
+	float3 boxSizeOffset = (this->box_.Size()) / 2.0f;
+	AABB subBox;
+	
+	subBox.maxPoint = float3(maxPoints.x - boxSizeOffset.x, maxPoints.y, maxPoints.z - boxSizeOffset.z);
+	subBox.minPoint = float3(minPoints.x , minPoints.y, minPoints.z );
+	child_nodes_.push_back(new CustomQuadTreeNode(subBox,this));
+	
+	subBox.maxPoint = float3(maxPoints.x, maxPoints.y, maxPoints.z - boxSizeOffset.z);
+	subBox.minPoint = float3(minPoints.x + boxSizeOffset.x, minPoints.y, minPoints.z);
+	child_nodes_.push_back(new CustomQuadTreeNode(subBox, this));
+
+	subBox.maxPoint = float3(maxPoints.x - boxSizeOffset.x, maxPoints.y, maxPoints.z);
+	subBox.minPoint = float3(minPoints.x, minPoints.y, minPoints.z + boxSizeOffset.z);
+	child_nodes_.push_back(new CustomQuadTreeNode(subBox, this));
+
+	subBox.maxPoint = float3(maxPoints.x, maxPoints.y, maxPoints.z);
+	subBox.minPoint = float3(minPoints.x + boxSizeOffset.x, minPoints.y, minPoints.z + boxSizeOffset.z);
+	child_nodes_.push_back(new CustomQuadTreeNode(subBox, this));
+}
