d89c05c7cbafb16a0e9a327e2570adbe015728dc
Franky Pear <FrankPear85@hotmail.com>
2018-02-12 00:31:16 +0100

    Adding BROFILER

--- YunitiTresde/ModuleImGui.cpp
+++ YunitiTresde/ModuleImGui.cpp
@@ -4,11 +4,13 @@
 
 #include "imgui-1.53\imgui.h"
 #include "imgui-1.53\imgui_impl_sdl_gl3.h"
+#include "Brofiler/include/Brofiler.h"
 #include "Application.h"
 #include "ModuleImGui.h"
 #include "ModuleWindow.h"
 #include "ModuleRenderer.h"
 #include "ModuleScene.h"
+#pragma comment( lib, "Brofiler/libx86/ProfilerCore32.lib")
 
 ModuleImGui::ModuleImGui()
 {
@@ -114,6 +116,7 @@ void ModuleImGui::AboutImgui()
 			ImGui::Text("File: %s", selected_item_);
 			ImGui::Text("FORMAT: %d", format_);
 		}
+		BROFILER_CATEGORY("CHECK STATUS", Profiler::Color::GreenYellow);
 		ImGui::Text("CPUs: %i (Cache: %iKB)", SDL_GetCPUCount(), SDL_GetCPUCacheLineSize());
 		ImGui::Text("System RAM: %iGB", SDL_GetSystemRAM() / 1000);
 		ImGui::Text("CollissionwithQuadTree: %d", App->scene->imguiCollisionTestQuadtree++);
