4b07de0c375d82f202978cb5b122f4aea52485ff
Max72590 <max-725@hotmail.com>
2018-02-28 21:34:26 +0100

    Updated anim module with data structures.

--- YunitiTresde/ModuleAnimation.cpp
+++ YunitiTresde/ModuleAnimation.cpp
@@ -10,3 +10,39 @@ ModuleAnimation::ModuleAnimation()
 ModuleAnimation::~ModuleAnimation()
 {
 }
+
+bool ModuleAnimation::Init()
+{
+
+}
+
+bool ModuleAnimation::Start()
+{
+
+}
+
+bool ModuleAnimation::CleanUp()
+{
+
+}
+
+update_status ModuleAnimation::PreUpdate(float dt)
+{
+
+}
+
+update_status ModuleAnimation::Update(float dt)
+{
+
+}
+
+update_status ModuleAnimation::PostUpdate(float dt)
+{
+
+}
+
+void ModuleAnimation::Load(aiString name, const char* filepath)
+{
+
+}
+
--- YunitiTresde/ModuleAnimation.h
+++ YunitiTresde/ModuleAnimation.h
@@ -1,26 +1,76 @@
 #pragma once
 #include "Module.h"
+#include "assimp\assimp\cimport.h"
+#include "assimp\assimp\postprocess.h"
+#include "assimp\assimp\scene.h"
+#include "assimp\assimp\Importer.hpp"
+#include <map>
+#include <vector>
+
+struct LessString {
+	bool operator() (const aiString& a, aiString& b) const {
+		return a.C_Str < b.C_Str;
+	}
+};
 
 struct NodeAnim
 {
-
+	aiString animNodeName = aiString("default-name");
+	aiVector3D* positionArray = nullptr;
+	uint numPositions = 0;
+	aiQuaternion* rotationArray = nullptr;
+	uint numRotations = 0;
 };
 
 struct Anim
 {
-
+	NodeAnim *channelArray= nullptr;
+	uint numChannels = 0;
+	uint animDuration = 0;
 };
 
 struct AnimInstance 
 {
+	Anim *animation = nullptr;
+	uint time = 0;
+	bool loop = false;
 
+	AnimInstance *nextAnim = nullptr;
+	uint blend_duration = 0;
+	uint blend_time = 0;
 };
 
 class ModuleAnimation :
 	public Module
 {
+	typedef std::map<aiString, Anim*, LessString> AnimMap;
+	typedef std::vector<AnimInstance*> InstanceList;
+	typedef std::vector<uint> HoleList;
+
+	AnimMap animations;
+	InstanceList instances;
+
 public:
 	ModuleAnimation();
 	~ModuleAnimation();
+
+	bool Init();
+	bool Start();
+	bool CleanUp();
+
+	update_status PreUpdate(float dt);
+	update_status Update(float dt);
+	update_status PostUpdate(float dt);
+
+	void Load(aiString name, const char* filepath);
+	uint Play(const char* name);
+	void Stop(uint id);
+	void BlendTo(uint id, const char* name, uint blend_time);
+
+	bool GetTransform(uint animId, const char* channel, aiVector3D& position, aiQuaternion& rotation);
+	
+public:
+	uint AnimId;
+
 };
 
